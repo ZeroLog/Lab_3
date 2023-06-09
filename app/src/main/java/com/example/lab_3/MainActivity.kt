53c9e15af99cd0abf2f30d50c757be71b9ce7163
ZeroLog <zerologyt@gmail.com>
2023-06-09 15:10:44 +0700

    test1

--- app/src/main/java/com/example/lab_3/MainActivity.kt
+++ app/src/main/java/com/example/lab_3/MainActivity.kt
@@ -3,12 +3,23 @@ package com.example.lab_3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.node.modifierElementOf
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab_3.ui.theme.Lab_3Theme
 
@@ -16,31 +27,48 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Lab_3Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Lab_3Theme {
-        Greeting("Android")
+
+Row(modifier = Modifier
+    .background(Color.Cyan).fillMaxWidth(1f).fillMaxHeight(0.7f)) {
+
+    Row(
+        modifier = Modifier
+            .background(Color.Red),
+
+    ) {
+
+        Text(text = "Question")
     }
+
+    Column(
+        modifier = Modifier
+            .background(Color.Cyan),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.End
+    ) {
+
+
+        Text(text = "False")
+    }
+    Column(modifier = Modifier
+        .background(Color.Cyan),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.End) {
+        Text(text = "True")
+        Text(text = "Next")
+    }
+
+}
 }
